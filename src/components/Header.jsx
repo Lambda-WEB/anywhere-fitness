68e1c516a3261fe3cfc93f9b9c2b07bed3dafe46
NaJ <naj10000@live.com>
2020-04-28 12:13:38 -0400

    header linked

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,32 @@
+
 import React from 'react'
 import logo from '../assets/logo.svg';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
-export default function Header() {
+
+export default function Header(props) {
 
   return (
-    <header >
-      <img src={logo} className="App-logo" alt="logo" />
-      
-      <nav className='header-navigation'>
+    <header className="app-header">
+      <div className='header-section'>
+        <img src={logo} className="app-logo-img" alt="logo" />
+      </div>
+      <div className="header-section">
+        <span className="header-title"><strong>Anywhere Fitness</strong></span>
+      </div>
+      <nav className='header-section'>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/classes'>Find Classes</Link>
+          </li>
+          <li>
+            {props.loggedIn ? (<Link to='/logout'>Logout</Link>) : (<Link to='/login'>Login</Link>)}
+          </li>
+        </ul>
       </nav>
-     
-     
     </header>
   )
 
