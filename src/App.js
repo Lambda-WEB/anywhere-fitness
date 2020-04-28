a8a499c485d56d977aec08a57970c56ca42d25d7
Anthony K <antonyk@users.noreply.github.com>
2020-04-27 23:16:37 -0400

    tweaked header styles; added baseline routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import React from 'react';
-import './styles/App.scss';
+import { Switch, Route } from 'react-router-dom'
+
 import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
       <Header />
+
+      <section>
+        <Switch>
+          <Route exact path='/login'>
+          </Route>
+          <Route exact path='/signup'>
+          </Route>
+          <Route path='/classes'>
+          </Route>
+          <Route path='/profile/edit'>
+          </Route>
+          <Route path='/profile'>
+          </Route>
+          <Route path='/instructor/classes/edit/:id'>
+          </Route>
+          <Route path='/instructor/classes/new'>
+          </Route>
+          <Route path='/instructor/classes'>
+          </Route>
+
+        </Switch>
+      </section>
     </div>
   );
 }
