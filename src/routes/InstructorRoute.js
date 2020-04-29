dffe27a33683e2329fe0ec240052eaef641c304a
Anthony K <antonyk@users.noreply.github.com>
2020-04-29 10:42:44 -0400

    private route fix? -- still fishy

--- src/routes/InstructorRoute.js
+++ src/routes/InstructorRoute.js
@@ -8,10 +8,19 @@ const InstructorRoute = ({ component: Component, ...rest})=> {
    
     return (
         <Route {...rest} render={props => {
-          if (loggedIn && isInstructor) {
-            return <Component {...props} />
-          }
-          else {
+          console.log('logged:',loggedIn,', instructor:',isInstructor)
+          if (loggedIn) {
+            if (isInstructor) {
+              return <Component {...props} />
+            } else {
+              return (
+                <p>
+                  You do not have access to this section of the website!
+                  You must be an instructor in order to create a new class!
+                </p>
+              )
+            }
+          } else {
             return <Redirect to="/login" />
           }
         }}/>
