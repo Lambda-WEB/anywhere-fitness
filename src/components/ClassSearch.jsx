c03a657616c402369416f6d10e083e9e2e7be3f9
Anthony K <antonyk@users.noreply.github.com>
2020-04-30 14:40:25 -0400

    created search and filtering algorithms; persist search and filtering values in global state

--- src/components/ClassSearch.jsx
+++ src/components/ClassSearch.jsx
@@ -1,19 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux';
 
 
 export default function ClassSearch() {
+  const dispatch = useDispatch();
+  const [searchVal, setSearchVal] = useState('');
+
+  function submitHandler(e) {
+    e.preventDefault();
+    dispatch({ type: 'CLASSES_SEARCH_UPDATE', payload: searchVal })
+  }
 
   return (
     <>
 
     <form onSubmit={submitHandler} >
 
-      <label htmlFor="instructor">Instructor Name
-        <input name='instructor' type="text" id='instructor' className='' 
-          placeholder='Enter Name' onChange={changeHandler} />
+      <label htmlFor="search">
+        <input name='search' type="search" id='search' className='' 
+          placeholder='Search for Classes or Instructors' onChange={(e) => setSearchVal(e.target.value)} />
       </label>
-
-
 
     </form>
 
--- src/store/classesReducer.js
+++ src/store/classesReducer.js
@@ -1,7 +1,22 @@
 // import * as act from './actions';
 // MANAGES THE STATE OF THE SEARCHABLE CLASSES LIST
 
+const initialFiltersState =  {
+  name: '', // string
+  type: '', // class_type
+  intensity: '', // class_intensity
+  start_date: '', // string: date
+  start_time: '', // string: time
+  duration: '', // number (minutes)
+  location: '', // string: zip code
+  attendees: '', // number (calculated)
+  max_attendees: '', // number 
+  instructor: '', // string: check against first/last name
+  instructorId: '', // number: for strict filtering
+};
+
 const initialClassesState = {
+    raw_list: [],
     list: [],
     class: {
       id: 0,
@@ -14,17 +29,8 @@ const initialClassesState = {
       attendees: '', // number (calculated)
       max_attendees: '', // number (specified by Instructor)
     },
-    filters: {
-      id: 0,
-      name: '', // string
-      type: '', // class_type
-      start_time: '', // string (utc)
-      duration: '', // number (minutes)
-      intensity: '', // class_intensity
-      location: '', // string: zip code
-      attendees: '', // number (calculated)
-      max_attendees: '', // number (specified by Instructor)
-    },
+    filters: initialFiltersState,
+    search: '', // string: against class name, instructor name
     isFetching: false
   }
 
@@ -41,10 +47,56 @@ export function classesReducer(state = initialClassesState, action) {
     case 'CLASSES_LIST_UPDATE':
       return {
         ...state,
-        list: action.payload,
+        raw_list: action.payload,
+        list: narrowClassList(action.payload, state.filters, state.search),
       }
-    
+
+    case 'CLASSES_FILTERS_UPDATE':
+      return {
+        ...state,
+        list: narrowClassList(state.raw_list, action.payload, state.search),
+        filters: action.payload,
+      }
+
+    case 'CLASSES_SEARCH_UPDATE':
+      return {
+        ...state,
+        list: narrowClassList(state.raw_list, state.filters, action.payload),
+        search: action.payload,
+      }
+
+    case 'CLASSES_LIST_RESET':
+      return {
+        ...state,
+        list: state.raw_list,
+        filters: initialFiltersState,
+        search: '',
+      }
+        
       default:
         return state;
     }
+}
+
+function narrowClassList(list, filters, search) {
+  return searchClassList(filterClassList(list, filters), search)
+}
+
+function filterClassList(list, filters) {
+  return list.filter(item => {
+    Object.keys(filters).forEach(f => {
+      if (filters[f] == true)
+        return (item[f] == filters[f])
+    })
+  })
+}
+
+function searchClassList(list, search) {
+  const searchArr = search.split(' ');
+  return list.filter(item => {
+    return (
+      item.name.includes(searchArr) ||
+      item.instructorName.first_name.includes(searchArr) ||
+      item.instructorName.last_name.includes(searchArr))
+  })
 }
